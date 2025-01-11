c326981b95b49ac64aac6bb37257788051ce35db
Baptistedupre <102721949+Baptistedupre@users.noreply.github.com>
2025-01-11 15:02:35 +0100

    TransformerTTS init

---
+++ Synthesizer/model/models.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+from .modules import Encoder, Decoder
+
+
+class TransformerTTS(nn.Module):
+    def __init__(self):
+        super(TransformerTTS, self).__init__()
+
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, src_seq, src_pos, mel_pos, mel_input):
+        enc_output, enc_mask, attention_enc = self.encoder(src_seq, src_pos)
+        mel_output, out, gate_output, attention_dec, attention_enc_dec = self.decoder(enc_output, mel_pos, mel_input) # noqa E501
+
+        output = mel_output, out, gate_output, attention_dec, attention_enc_dec, attention_enc # noqa E501 
+
+        return output
