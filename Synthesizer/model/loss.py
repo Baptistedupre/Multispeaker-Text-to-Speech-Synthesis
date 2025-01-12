6ac332795d097dbd51f7e88275ed16c4e24c09a9
Baptistedupre <102721949+Baptistedupre@users.noreply.github.com>
2025-01-12 19:22:36 +0100

    loss init

---
+++ Synthesizer/model/loss.py
@@ -0,0 +1,23 @@
+from torch import nn
+
+
+class TransformerTTSLoss(nn.Module):
+    def __init__(self, r_gate=5.):
+        super(TransformerTTSLoss, self).__init__()
+
+        self.mel_loss = nn.MSELoss()
+        self.gate_loss = nn.BCEWithLogitsLoss()
+        self.r_gate = r_gate
+
+    def forward(self, mel_output, targets):
+        mel_target, gate_target = targets[0], targets[1]
+        mel_target.requires_grad = False
+        gate_target.requires_grad = False
+
+        mel_out_postnet, mel_out, gate_out = mel_output
+        gate_out = gate_out.view(-1, 1)
+
+        mel_loss = self.mel_loss(mel_out, mel_target)
+        gate_loss = self.gate_loss(gate_out, gate_target) * self.r_gate
+
+        return mel_loss + gate_loss
